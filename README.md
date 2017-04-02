fb752a295b903a20443a739bb6355c3ebd8ebbdf
Michael Smith <iimeetmikeii@gmail.com>
2017-04-03 11:03:41 +1200

    added all files

---
+++ README.md
@@ -0,0 +1,9 @@
+# Build Tools
+
+## JSHint
+
+Watches JS files in JS folder and lints JS
+
+run: `cd into/your-project`
+run: `npm install`
+run: `npm run build`
---
+++ jshint/js/custom.js
@@ -0,0 +1,32 @@
+var button, more, height, visible;
+
+more = document.querySelector('.more');
+  //Get height of .more element
+height = more.clientHeight;
+  //console.log(height)
+
+//Hide content with css
+more.style.cssText = 'max-height: 0; overflow: hidden;';
+
+button = document.createElement('button');
+button.classList.add('show-more');
+button.innerHTML = 'Read more...';
+  //Insert button after .more element
+more.parentNode.insertBefore(button, more.nextSibling);
+
+button = document.querySelector('.show-more');
+
+visible = false;
+
+button.addEventListener('click', function() {
+  if (!visible) {
+  	button.innerHTML = 'Read less...';
+    more.style.transition = '1s';
+    more.style.maxHeight = height + 'px';
+  } else {
+  	button.innerHTML = 'Read more...';
+    more.style.maxHeight = 0;
+  }
+  //Alternate between false and true
+  visible = !visible;
+});
